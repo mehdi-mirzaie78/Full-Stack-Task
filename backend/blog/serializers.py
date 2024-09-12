834d84e790dcf6d319d5e216b9c3b2b3382705fe
specter <mehdimirzaie1378@gmail.com>
2024-09-12 13:11:37 +0330

    backend | added new permission for the blog owner

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -8,8 +8,9 @@ class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = ("id", "user", "title", "content", "image", "created_at", "updated_at")
+        extra_kwargs = {"image": {"required": False, "allow_empty_file": False}}
 
-    def save(self, **kwargs):
+    def save(self):
         user = self.context["request"].user
         self.validated_data["user"] = user
-        return super().save(**kwargs)
+        return super().save()
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, SAFE_METHODS, AllowAny
+
 from .models import Blog
 from .serializers import BlogSerializer
+from .permissions import IsBlogOwner
 
 
 class BlogViewSet(ModelViewSet):
@@ -11,8 +13,19 @@ class BlogViewSet(ModelViewSet):
     def get_permissions(self):
         if self.request.method in SAFE_METHODS:
             return [AllowAny()]
-        return [IsAuthenticated()]
+        if self.request.method == "POST":
+            return [IsAuthenticated()]
+        return [IsAuthenticated() and IsBlogOwner()]
+
+    def update(self, request, *args, **kwargs):
+        blog: Blog = self.get_object()
+        self.check_object_permissions(request, blog)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        blog: Blog = self.get_object()
+        self.check_object_permissions(request, blog)
+        return super().destroy(request, *args, **kwargs)
 
     def get_serializer_context(self):
-        return {'request': self.request}
-
+        return {"request": self.request}
